b274405d671e18b26cc4b21a0509c41c5dac569c
Yann Salmon <coolcode.ys@gmail.com>
2020-04-22 10:24:15 +0200

    Creating doc in db for user when authenticated

--- src/components/contexts/AuthContext.jsx
+++ src/components/contexts/AuthContext.jsx
@@ -1,17 +1,34 @@
 import React, { useState } from 'react'
-import { auth } from '../../scripts/init_firebase'
+import { auth, db } from '../../scripts/init_firebase'
 const AuthContext = React.createContext()
 
 /**
- * @file Initiates the authentification context
+ * @file Initiates the authentification context and user doc in db
  * @requires react
  */
 const AuthContextProvider = ({ children }) => {
     const [userCred, setUserCred] = useState(null);
 
+    // Listen to auth changes
     auth.onAuthStateChanged(userCred => {
         setUserCred(() => userCred)
     });
+
+    // Initialize user doc in db if not already done
+    if (userCred) {
+        const userDocRef = db.collection("users").doc(userCred.uid);
+
+        userDocRef.get().then(userDoc => {
+            if (!userDoc.exists) {
+                userDocRef.set({
+                    quotes: [],
+                    colors: []
+                })
+            }
+        }).catch(err => {
+            console.error("Error getting userDoc:", err)
+        })
+    }
 
     return (
         <AuthContext.Provider
