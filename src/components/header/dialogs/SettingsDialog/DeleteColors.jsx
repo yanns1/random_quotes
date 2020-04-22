0be7d3fe9058b7a99c06e6a3886e64b3b56a33de
Yann Salmon <coolcode.ys@gmail.com>
2020-04-22 20:31:54 +0200

    in progress

--- src/components/header/dialogs/SettingsDialog/DeleteColors.jsx
+++ src/components/header/dialogs/SettingsDialog/DeleteColors.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect, useContext } from 'react'
+import { getEl } from '../../../../scripts/utils.js'
+import { firebase, db } from '../../../../scripts/init_firebase.js'
+import { AuthContext } from '../../../contexts/AuthContext.jsx'
 
 const DeleteColors = ({
     colorDeletedMess,
-    setColorDeletedMess
+    setColorDeletedMess,
+    isErrorMess
 }) => {
+    // Contexts
+    const { userCred } = useContext(AuthContext)
+    // States
+    const [colors, setColors] = useState([])
+
+    /**
+     * Listen to db and set colors state accordingly
+     * @function getStats
+     * @returns {function} - unsuscribe function
+     */
+    const getStats = () => {
+        const userDocRef = db.collection("users").doc(userCred.uid)
+        const unsubscribe = userDocRef.onSnapshot(userDoc => {
+            setColors(() => userDoc.data().colors)
+        }, err => {
+            console.error(`Error during listening for colors: ${err}`)
+        })
+
+        return unsubscribe
+    }
+
+    /**
+     * Start listening to db for colors
+     */
+    useEffect(() => {
+        const unsubscribe = getStats()
+
+        return () => {
+            unsubscribe()
+        }
+    }, [])
+
+    console.log(colors)
+    /**
+     * @todo
+     * map over colors and return JSX for a checkbox combined with a div with background set to color
+     */
     const colorCheckboxes = []
     return (
         <>
