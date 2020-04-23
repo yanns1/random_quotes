bdae1986610baeaf9cf3c913200adb427d2a1f4d
Yann Salmon <coolcode.ys@gmail.com>
2020-04-23 11:14:53 +0200

    Implementing Quote card logic

--- src/components/main/QuoteCard.jsx
+++ src/components/main/QuoteCard.jsx
@@ -1,95 +1,146 @@
 import React, { useState, useEffect, useContext } from 'react'
-import { db } from '../../scripts/init_firebase'
+import { getJSON, pickRandomInArr } from '../../scripts/utils.js'
+import { db, firebase } from '../../scripts/init_firebase.js'
 import { AuthContext } from '../contexts/AuthContext.jsx'
 
 const QuoteCard = () => {
     // Contexts
     const { userCred } = useContext(AuthContext)
-    // console.log(userCred)
-    // const [text, setText] = useState('');
-    // const [author, setAuthor] = useState('');
-    const [quotes, setQuotes] = useState([])
+    // States
+    const [text, setText] = useState('')
+    const [author, setAuthor] = useState('')
+    const [color, setColor] = useState('')
+    const [quoteDeletedMess, setQuoteDeletedMess] = useState('')
 
-    // function chooseRandomly(array) {
-    //     const randomIndex = Math.round(Math.random() * (array.length - 1));
-    //     return array[randomIndex];
-    // }
+    const isErrorMess = mess => /^Error/.test(mess)
 
-    const getQuote = () => {
+    /**
+     * Set random quote and random color as states, from db data
+     * @function getQuoteAndColor
+     * @returns {void}
+     */
+    const getDataFromDb = () => {
         const userDocRef = db.collection("users").doc(userCred.uid)
-        userDocRef.get().then(doc => {
-            // console.log(doc)
-            if (doc.exists) {
-                setQuotes(() => doc.data().quotes)
-            } else {
-                console.error(`userDoc doesn't exist`)
+        userDocRef.get().then(userDoc => {
+            if (!userDoc.exists) {
+                throw `userDoc doesn't exist`
             }
+            const randomQuote = pickRandomInArr(userDoc.data().quotes)
+            const randomColor = pickRandomInArr(userDoc.data().colors)
+            setText(() => randomQuote.quote)
+            setAuthor(() => randomQuote.author)
+            setColor(() => randomColor)
+        }).catch(err => {
+            console.error(`Error getting userDoc when trying to get a new quote: ${err}`)
         })
-
-        // return {
-        //     quote: ,
-        //     author:
-        // }
     }
 
-    if (userCred) {
-        getQuote()
+    const getPlaceholderData = async () => {
+        // absolute path is needed
+        const url = "/src/data/placeholder_data.json"
+        try {
+            const placeholderData = await getJSON(url)
+            const randomQuote = pickRandomInArr(placeholderData.quotes)
+            const randomColor = pickRandomInArr(placeholderData.colors)
+            setText(() => randomQuote.quote)
+            setAuthor(() => randomQuote.author)
+            setColor(() => randomColor)
+        } catch (err) {
+            console.error(`Error fetching placeholder data: ${err}`)
+        }
     }
 
-    // function changeQuote() {
-    //     quoteCollection.onSnapshot(snapshot => {
-    //         let quotesArray = [];
-    //         snapshot.docChanges().forEach(change => {
-    //             const quoteObj = change.doc.data();
-    //             quotesArray.push(quoteObj);
-    //         });
-    //         const randomQuote = chooseRandomly(quotesArray);
-    //         setText(() => randomQuote.quote);
-    //         setAuthor(() => randomQuote.author);
-    //     })
-    // }
+    /**
+     * Choose from where to get data depending on userCred + reset message from quote deletion
+     * @function getData
+     * @returns {void}
+     */
+    const getData = () => {
+        if (userCred) {
+            getDataFromDb()
+        } else {
+            getPlaceholderData()
+        }
 
-    // function changeColor() {
-    //     colorCollection.onSnapshot(snapshot => {
-    //         let colorsArray = [];
-    //         snapshot.docChanges().forEach(change => {
-    //             const color = change.doc.data().value;
-    //             colorsArray.push(color);
-    //         });
-    //         const randomColor = chooseRandomly(colorsArray);
-    //         const root = document.documentElement;
-    //         root.style.setProperty('--primary', randomColor);
-    //     })
-    // }
+        setQuoteDeletedMess(() => '')
+    }
 
-    // function handleClick(e) {
-    //     const { id } = e.target;
-    //     if (id === 'button-quote') {
-    //         changeQuote();
-    //         changeColor();
-    //     }
-    // }
+    /**
+     * Change page primary color depending on color state
+     */
+    const changeColor = () => {
+        const root = document.documentElement
+        root.style.setProperty('--primary', color)
+    }
 
-    // useEffect(() => {
-    //     // absolute path obligatoirement dans fetch(). Donc '/' pour partir du nom de domaine.
-    //     changeQuote();
-    //     changeColor();
-    // }, []);
+    const deleteQuote = e => {
+        const quoteToRemove = {
+            quote: e.target.dataset.text,
+            author: e.target.dataset.author
+        }
+        const userDocRef = db.collection("users").doc(userCred.uid)
+        db.runTransaction(transaction => {
+            return transaction.get(userDocRef).then(userDoc => {
+                if (!userDoc.exists) {
+                    throw "userDoc does not exist"
+                }
+                transaction.update(userDocRef, {
+                    "quotes": firebase.firestore.FieldValue.arrayRemove(quoteToRemove)
+                })
+            })
+        })
+        .then(() => {
+            setQuoteDeletedMess(() => 'Quote successfully deleted !')
+        })
+        .catch(err => {
+            setColorAddedMess(() => 'Error: Quote has not been deleted !')
+            console.error(`Error during transaction for deleting quote (either getting doc or updating it): ${err}`)
+        })
+    }
+    /**
+     * Set text and author at first render, otherwise nothing appears
+     */
+    useEffect(() => {
+        getData()
+    }, [userCred])
 
-    return null
-    // return (
-    //     <div id="quote-box">
-    //         <div id="text">
-    //             <i className="fas fa-quote-left"></i>
-    //             {text}
-    //         </div>
-    //         <div id="author">- {author}</div>
-    //         <div id="new-quote">
-    //             <a href={"https://twitter.com/intent/tweet?text=" + encodeURIComponent('"' + text + '"  ' + '-' + author)} target="_blank" id="tweet-quote" title="Tweet this quote!"><i className="fab fa-twitter" /></a>
-    //             <button id="button-quote" onClick={handleClick}>New Quote</button>
-    //         </div>
-    //     </div>
-    // )
+    useEffect(() => {
+        changeColor()
+    }, [color])
+
+    return (
+        <>
+            {userCred
+                ? null
+                : <div className="not-authenticated">
+                    You want to choose your own quotes and colors ? Sign in !
+            </div>
+            }
+            <div className="quote-card">
+                <div className="quote-text">
+                    <i className="fas fa-quote-left"></i>
+                    {text}
+                </div>
+                <div className="quote-author">- {author}</div>
+                <div className="quote-footer">
+                    <a href={"https://twitter.com/intent/tweet?text=" + encodeURIComponent('"' + text + '"  ' + '-' + author)} target="_blank" className="quote-tweet" title="Tweet this quote!"><i className="fab fa-twitter" /></a>
+                    {userCred
+                        ?
+                        <>
+                            <div className="delete-icon material-icons" data-text={text} data-author={author} onClick={deleteQuote}>delete</div>
+                            {quoteDeletedMess
+                                ?
+                                <div className={isErrorMess(quoteDeletedMess) ? 'err-mess' : 'success-mess'}>{quoteDeletedMess}</div>
+                                : null
+                            }
+                        </>
+                        : null
+                    }
+                    <button className="quote-new" onClick={getData}>New Quote</button>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default QuoteCard
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -4,4 +4,13 @@ const getEl = sel => document.querySelector(sel)
 // getEls :: String -> [DOM Element]
 const getEls = sel => document.querySelectorAll(sel)
 
-export { getEl, getEls }
+// getJSON :: String -> Promise(Object)
+const getJSON = async (url) => {
+    const res = await fetch(url)
+    return res.json()
+}
+
+// pickRandomInArr :: [a] -> a
+const pickRandomInArr = arr => arr[Math.round(Math.random() * (arr.length - 1))]
+
+export { getEl, getEls, getJSON, pickRandomInArr }
