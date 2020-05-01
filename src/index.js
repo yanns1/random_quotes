43dbb02ec429c556c538cca9d93be31c36bf3c28
Yann Salmon <coolcode.ys@gmail.com>
2020-05-01 10:00:19 +0200

    Remove PWA stuff and ramda

--- src/index.js
+++ src/index.js
@@ -11,18 +11,4 @@ import App from './components/App.jsx';
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
-);
-
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/src/sw.js')
-            .then(registration => {
-                // Registration was successful
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            })
-            .catch(err => {
-                // registration failed :(
-                console.log('ServiceWorker registration failed: ', err);
-            })
-    });
-}
+)
